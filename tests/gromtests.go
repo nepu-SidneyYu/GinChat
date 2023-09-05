6c20f5f5f12a6dcccb3d05c493ce6054b9134989
yyx <yuyinxi@able-elec.com>
2023-09-05 09:40:41 +0800

    mysql练习增删改

--- tests/gromtests.go
+++ tests/gromtests.go
@@ -9,21 +9,22 @@ import (
 )
 
 type User struct {
-	ID    int64  `gorm:"column:id"`
-	Name  string `gorm:"column:name"`
-	Email string `gorm:"column:phone"`
+	ID    int64  //`gorm:"column:id"`
+	Name  string //`gorm:"column:name"`
+	Phone string //`gorm:"column:phone"`
 }
 
 func main() {
 	cfg := mysql.Config{
-		User:                 "root",   //os.Getenv("DBUSER"),
-		Passwd:               "123456", //os.Getenv("DBPASS"),
+		User:                 "root",
+		Passwd:               "123456",
 		Net:                  "tcp",
 		Addr:                 "127.0.0.1:3306",
-		DBName:               "users",
+		DBName:               "test",
 		AllowNativePasswords: true,
 	}
 	db, err := sql.Open("mysql", cfg.FormatDSN())
+	defer db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,13 +32,23 @@ func main() {
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
+
+	//users := []*User{
+	//	&User{ID: 9385, Name: "sjgs", Phone: "hjsdjf"},
+	//	&User{ID: 8752, Name: "dskmf", Phone: "skgs"},
+	//}
+	//db.Exec("insert into user values (9385,'sjgs','hjsdjf')")
+	//db.Exec("insert into user values (8752,'dskmf','skgs')")
 	var user User
+	//deletedb, err := db.Prepare("delete from user where id=?")
+	//deletedb.Exec(9385)
+	//deletedb.Exec(8752)
 	rows, e := db.Query("select * from user")
-	if e == nil {
+	if e != nil {
 		errors.New("query incur error")
 	}
 	for rows.Next() {
-		rows.Scan(&user.ID, &user.Name, &user.Email)
+		rows.Scan(&user.ID, &user.Name, &user.Phone)
 		fmt.Printf("%#v\n", user)
 	}
 	rows.Close()
